a498ec74b8eeb971fe32fa675a24b95be4920f58
simon0820s <simon0820s@gmail.com>
2023-06-04 11:51:15 -0500

    create scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,17 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="flex flex-col justify-center items-center mt-5"
+        >
           <span className="w-5 h-5 rounded-full bg-[#915eff]" />
           <span className="w-1 sm:h-80 h-40 mb-1 violet-gradient" />
-        </div>
+        </motion.div>
 
-        <div className="">
+        <div>
           <h1 className={`${styles.heroHeadText} text-neutral-100`}>
             Hi, I'm <span className="text-[#915eff]">Simón</span>
           </h1>
@@ -24,6 +29,23 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-[#915eff92] flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                delay: 2,
+                repeat: Infinity
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
