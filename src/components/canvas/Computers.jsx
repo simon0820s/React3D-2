512c4817526accfd088bcbd2e526507b14381bfb
simon0820s <simon0820s@gmail.com>
2023-06-04 10:52:35 -0500

    repair load error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -13,8 +13,8 @@ const Computers = () => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={0.70}
+        position={[0, -2.50, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
